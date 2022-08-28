81ab53400a563fe47a4c71c9f1abd18ecb0a4d5b
Miguel Eq <equihuam@gmail.com>
2022-08-28 10:48:18 -0500

    Producción de datos de IIE agregados por estado(promedio)

--- Scripts/Convierte shape estados a raster.R
+++ Scripts/Convierte shape estados a raster.R
@@ -3,7 +3,8 @@ library(tidyverse)
 library(sf)
 library(terra)
 library(tidyterra)
-#library(raster)
+library(RColorBrewer)
+library(paletteer)
 
 
 # Ubicación archivo "ESRI-shape" de estados de la república mexicana
@@ -25,6 +26,10 @@ cat(crs(ie2018_r_tr))
 plot(ie2018_r_tr)
 lines(edos_shp_tr)
 
+# Tipo de datos en estos mapas
+class(edos_shp_tr)
+class(ie2018_r_tr)
+
 # Geometría
 crs(edos_shp_tr, describe=TRUE)$area
 
@@ -32,7 +37,7 @@ crs(edos_shp_tr, describe=TRUE)$area
 crs(ie2018_r_tr)  <- "epsg:6372"
 crs(edos_shp_tr)    <- "epsg:6372"
 crs(edos_shp_tr, describe=TRUE)$area
-
+ext(edos_shp_tr)
 
 ### SpatVector: selección de objetos geográficos
 edos_shp_tr[1:3]$NOMGEO
@@ -40,12 +45,12 @@ plot(edos_shp_tr[1:3,])
 plot(edos_shp_tr[2, 3])
 quer_v <- subset(edos_shp_tr, edos_shp_tr$NOMGEO == "Querétaro", 
                 c("CVEGEO", "CVE_ENT", "NOMGEO"))
+
+# Características de los Resultados y mapa
+cat(crs(quer_v))
+ext(quer_v)
 quer_v$NOMGEO
 plot(quer_v, "NOMGEO")
-
-# Tipo de datos en estos mapas
-class(edos_shp_tr)
-class(ie2018_r_tr)
 
 ## Podemos extraer datos de un raster (aquí ie2018) con base en un polígono. 
 ie2018_quer <- tibble(edo = "Que", extract(ie2018_r_tr, 
@@ -57,32 +62,48 @@ crs(ie2018_quer_v)  <- "epsg:6372"
 plot(ie2018_quer_v, "ie_2018_st_v2")
 
 # Promedio
-ie2018_quer_prom <- extract(ie2018_r_tr, quer_v, na.rm=TRUE, fun = mean,
+ie2018_edo_prom_v <- extract(ie2018_r_tr, edos_shp_tr, na.rm=TRUE, fun = mean,
                             as.spatvector = TRUE, xy = TRUE, bind = TRUE)
-plot(ie2018_quer_prom, "ie_2018_st_v2")
+plot(ie2018_edo_prom_v, "ie_2018_st_v2")
+head(ie2018_edo_prom_v)
 
-extract(ie2018_r_tr, quer_v, na.rm=TRUE, as.spatvector = TRUE, 
-        xy = TRUE)
-
-# Mapa con más control de diseño
+# Mapa con más control de diseño mediante "ggplot2"
+# Mapa de estados de la república
 edos_shp_sf <- st_as_sf(edos_shp_tr)
 names(edos_shp_sf)
 ggplot(edos_shp_sf) + 
   geom_sf(aes(fill = NOMGEO), show.legend = FALSE) +
   labs(title = "México", subtitle = "objeto espacial sf a partir de <<SpatVector>>")
 
-r_puntos <- st_as_sf(as.points(ie2018_r_tr))
+# Integridad promedio por Estado
+ie2018_edo_prom_v_sf <- st_as_sf(ie2018_edo_prom_v)
+names(ie2018_edo_prom_v_sf)
+ggplot(ie2018_edo_prom_v_sf) + 
+  geom_sf(aes(fill = ie_2018_st_v2), show.legend = TRUE) +
+  labs(title = "México", subtitle = "Condición (IIE-2018 v2") +
+  scale_fill_gradient2(midpoint = 0.5, low = "firebrick", mid = "khaki1", 
+                       high = "darkolivegreen4", na.value = "transparent")
 
 # Mapas raster
 ggplot() +
   geom_spatraster(data = ie2018_r_tr) +
-  scale_fill_whitebox_c(palette = "muted", direction = -1)
+  labs(title = "México", subtitle = "IE-2018 v2 (objeto SpatRraster)")+
+  guides(fill = guide_legend(title="Condición (IIE)")) +
+  scale_fill_gradient2(midpoint = 0.5, low = "firebrick", mid = "khaki1", 
+                       high = "darkolivegreen4", na.value = "transparent")
 
+# Caso Querétaro
+quer_v_sf <- st_as_sf(quer_v)
 ie2018_quer_r <- rasterize(ie2018_quer_v, ie2018_r_tr, "ie_2018_st_v2")
-ggplot() +
-  geom_sf(edos_shp_sf, aes(colour = "black"))
+ext(ie2018_quer_r) <- ext(quer_v) 
+plot(ie2018_quer_r)
+lines(quer_v)
 
-ggplot(edos_shp_sf) +
+ext(ie2018_quer_r)
+
+ie2018_quer_r <- crop(ie2018_r_tr, quer_v)
+
+ggplot(quer_v_sf) +
   geom_sf(aes(), fill = "white", show.legend = FALSE) +
   geom_spatraster(data = ie2018_quer_r) +
   scale_fill_whitebox_c(palette = "muted", direction = -1)
