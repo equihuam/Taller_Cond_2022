212c51fe5f004218a94244dd30bc5ddb619c7029
Miguel Eq <equihuam@gmail.com>
2022-08-28 02:14:17 -0500

    Manejo de datos y mapeo con biblioteca terra

--- Scripts/Convierte shape estados a raster.R
+++ Scripts/Convierte shape estados a raster.R
@@ -2,11 +2,12 @@ library(ggplot2)
 library(tidyverse)
 library(sf)
 library(terra)
-library(raster)
+library(tidyterra)
+#library(raster)
 
 
 # Ubicación archivo "ESRI-shape" de estados de la república mexicana
-mapas_dir <- "V:/Datos/layers_harmon"
+mapas_dir <- "z:/Datos/layers_harmon"
 edos_arch <- list.files(mapas_dir, pattern = "^00.*shp$", full.names = TRUE)
 ie2018_r_arch <- list.files(mapas_dir, pattern = "^ie_2018.*.tif$", full.names = TRUE)
 
@@ -34,25 +35,34 @@ crs(edos_shp_tr, describe=TRUE)$area
 
 
 ### SpatVector: selección de objetos geográficos
-edos_shp_tr[1:3, 3]
+edos_shp_tr[1:3]$NOMGEO
 plot(edos_shp_tr[1:3,])
-plot(edos_shp_tr[1:2, 3])
+plot(edos_shp_tr[2, 3])
 quer_v <- subset(edos_shp_tr, edos_shp_tr$NOMGEO == "Querétaro", 
                 c("CVEGEO", "CVE_ENT", "NOMGEO"))
 quer_v$NOMGEO
-plot(quer_v)
+plot(quer_v, "NOMGEO")
 
 # Tipo de datos en estos mapas
 class(edos_shp_tr)
 class(ie2018_r_tr)
 
 ## Podemos extraer datos de un raster (aquí ie2018) con base en un polígono. 
-ie2018_quer <- tibble(edo = "Que", 
-                   ie2018 = sort(unique(extract(ie2018_r_tr, 
-                              quer_v, na.rm=TRUE)$ie_2018_st_v2)))
+ie2018_quer <- tibble(edo = "Que", extract(ie2018_r_tr, 
+                              quer_v, na.rm=TRUE, xy = TRUE))
+coordinates(ie2018_quer) <-  ~ x + y
+names(ie2018_quer)
+ie2018_quer_v <- vect(ie2018_quer)
+crs(ie2018_quer_v)  <- "epsg:6372"
+plot(ie2018_quer_v, "ie_2018_st_v2")
+
+# Promedio
 ie2018_quer_prom <- extract(ie2018_r_tr, quer_v, na.rm=TRUE, fun = mean,
                             as.spatvector = TRUE, xy = TRUE, bind = TRUE)
 plot(ie2018_quer_prom, "ie_2018_st_v2")
+
+extract(ie2018_r_tr, quer_v, na.rm=TRUE, as.spatvector = TRUE, 
+        xy = TRUE)
 
 # Mapa con más control de diseño
 edos_shp_sf <- st_as_sf(edos_shp_tr)
@@ -63,5 +73,16 @@ ggplot(edos_shp_sf) +
 
 r_puntos <- st_as_sf(as.points(ie2018_r_tr))
 
-ggplot(r_puntos) +
-  geom_sf(aes(col = zvh_inegi)) 
+# Mapas raster
+ggplot() +
+  geom_spatraster(data = ie2018_r_tr) +
+  scale_fill_whitebox_c(palette = "muted", direction = -1)
+
+ie2018_quer_r <- rasterize(ie2018_quer_v, ie2018_r_tr, "ie_2018_st_v2")
+ggplot() +
+  geom_sf(edos_shp_sf, aes(colour = "black"))
+
+ggplot(edos_shp_sf) +
+  geom_sf(aes(), fill = "white", show.legend = FALSE) +
+  geom_spatraster(data = ie2018_quer_r) +
+  scale_fill_whitebox_c(palette = "muted", direction = -1)
