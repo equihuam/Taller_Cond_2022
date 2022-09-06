2ec63b8cde2ad06190488708868b2571497c1eaf
Miguel Eq <equihuam@gmail.com>
2022-09-06 01:21:07 -0500

    Agregué acceso a Json para leer los datos de Miro

--- Scripts/Interacción con Miro.R
+++ Scripts/Interacción con Miro.R
@@ -1,14 +1,16 @@
 library(httr)
+library(jsonlite)
 
-id_tablero <- "uxjVPZm0dRE="
 id_tablero <- "uXjVPf-z5Tg="
 
-url <-  paste0("https://api.miro.com/v2/boards/", id_tablero)
+tablero <-  paste0("https://api.miro.com/v2/boards/", id_tablero)
 queryString <- list(limit = "10")
-response <- VERB("GET", url, 
+response <- VERB("GET", tablero, 
                  add_headers('Authorization' = 
                              'Bearer eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_hS_tz0Dughp2VY6nZCYbsTouuAg'), 
                  query = queryString, content_type("application/octet-stream"), 
                  accept("application/json"))
 
-content(response, "text")
+datos_tablero <- fromJSON(content(response, "text", encoding = "UTF-8"))
+
+datos_tablero$sharingPolicy
